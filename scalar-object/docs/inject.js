a5c03102f7f5ccc82cd04dc352e03e17aea81cd7
the.liquid.metal <the.liquid.metal@gmail.com>
2015-09-09 22:58:11 +0700

    rev-65

--- scalar-object/docs/inject.js
+++ scalar-object/docs/inject.js
@@ -16,13 +16,21 @@ $(document).ready(function(){
     $('#magicMethods').on('click', 'tr', function(){
         var $tr = $(this);
         var insertDetail = function(response){
-            var prefix = '<div class="ajax-area">';
-            var sufix = '</div>';
+            var $ajaxArea = $('<div class="ajax-area"/>');
             if (typeof response === 'string') {
-                $tr.find('.description.detailed').append(prefix + response + sufix);
+                $ajaxArea.append(response);
             } else {
-                $tr.find('.description.detailed').append(prefix + response.responseText + sufix);
+                $ajaxArea.append(response.responseText);
             }
+            $ajaxArea.find("code").each(function(){
+                var $code = $(this);
+                var $parent = $code.parent();
+                console.log($parent.prop('tagName'));
+                if ($parent.prop('tagName') === 'DIV') {
+                    $parent.addClass('code');
+                }
+            });
+            $tr.find('.description.detailed').append($ajaxArea);
             $tr.data('isLoaded', true);
         };
         if ($tr.data('isLoaded') === true) return;
