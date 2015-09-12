899b6697998e18b1a026d5bfa107ce0d85a8e429
the.liquid.metal <the.liquid.metal@gmail.com>
2015-09-12 15:24:19 +0700

    rev-83

--- scalar-object/docs/inject.js
+++ scalar-object/docs/inject.js
@@ -43,6 +43,7 @@ $(document).ready(function(){
                 newContent = oldContent
                     .replace(/<!--\?/g, '&lt;?')
                     .replace(/\?>/g, '?>')
+                    .replace(/=-->/g, '=>')
                     .replace(/\?-->/g, '?>');
 
                 indent = oldContent.match(/^\s+/);
